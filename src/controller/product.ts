411cbb9971cc50468713419eed1d36bad8068b2a
Sergio Quintana <sergioq15@hotmail.com>
2023-01-27 21:35:00 -0500

    refactor: change in the way the database is connected to improve performance

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { v4 as uuid } from 'uuid';
+import { database } from "../database";
 import Product from '../models/Products';
 
 
 export const createProduct = async (req: Request, res: Response) => {
+    await database.connect();
     const { name, description, urlImage, category, subCategory } = req.body;
 
     try {
@@ -17,12 +19,14 @@ export const createProduct = async (req: Request, res: Response) => {
         });
 
         await product.save();
+        await database.disconnect();
         return res.status(201).json({
             success: true,
             message: 'Product created successfully'
         });
 
     } catch (error) {
+        await database.disconnect();
         return res.status(500).json({
             success: false,
             message: error.message
@@ -32,16 +36,22 @@ export const createProduct = async (req: Request, res: Response) => {
 
 export const getProducts = async (_req: Request, res: Response) => {
     try {
+        await database.connect();
         const products = await Product.find({});
-        if (!products) return res.status(400).json({
-            success: false,
-            message: 'Products not found'
-        })
-
+        if (!products) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'Products not found'
+            })
+        }
+        await database.disconnect();
         return res.status(200).json({
             products
         })
+
     } catch (error) {
+        await database.disconnect();
         return res.status(500).json({
             success: false,
             message: error.message
@@ -52,16 +62,22 @@ export const getProducts = async (_req: Request, res: Response) => {
 export const getProduct = async (req: Request, res: Response) => {
     const { code } = req.params;
     try {
+        await database.connect();
         const product = await Product.findOne({ code });
-        if (!product) return res.status(400).json({
-            success: false,
-            message: 'Product not found'
-        });
+        if (!product) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'Product not found'
+            });
+        }
+        await database.disconnect();
         return res.status(200).json({
             product
         })
 
     } catch (error) {
+        await database.disconnect();
         return res.status(500).json({
             success: false,
             message: error.message
@@ -71,19 +87,26 @@ export const getProduct = async (req: Request, res: Response) => {
 
 export const updateProduct = async (req: Request, res: Response) => {
     try {
+        await database.connect();
+
         const { code } = req.params;
         /* const { name, description, urlImage, category, subCategory } = req.body; */
         const product = await Product.findOneAndUpdate({ code }, req.body, { new: true });
-        if (!product) return res.status(400).json({
-            success: false,
-            message: 'Product not found'
-        });
+        if (!product) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'Product not found'
+            });
+        }
+        await database.disconnect();
         return res.status(200).json({
             success: true,
             message: 'Product updated successfully',
             product
         });
     } catch (error) {
+        await database.disconnect();
         return res.status(500).json({
             success: false,
             message: error.message
@@ -93,17 +116,23 @@ export const updateProduct = async (req: Request, res: Response) => {
 
 export const deleteProduct = async (req: Request, res: Response) => {
     try {
+        await database.connect();
         const { code } = req.params;
         const product = await Product.findOneAndDelete({ code });
-        if (!product) return res.status(400).json({
-            success: false,
-            message: 'Product not found'
-        });
+        if (!product) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'Product not found'
+            });
+        }
+        await database.disconnect();
         return res.status(200).json({
             success: true,
             message: 'Product deleted successfully',
         });
     } catch (error) {
+        await database.disconnect();
         return res.status(500).json({
             success: false,
             message: error.message
--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -1,14 +1,50 @@
 import mongoose from "mongoose";
 
+const mongoConnection = {
+    isConnected: 0
+}
+
+
 mongoose.set("strictQuery", false);
-mongoose.connect(`${process.env.MONGO_URL}`)
+/* mongoose.connect(`${process.env.MONGO_URL}`)
     .then(() => console.log("Connected to database"))
     .catch(err => console.error(`Could not connect to MongoDB: ${err.message}`));
 
-export { mongoose };
+export { mongoose }; */
 
-/* mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
-    .then(() => console.log('Conexion correcta BD'))
-    .catch((err) => console.log(`ERROR to connect : ${err.message}`));
 
-module.exports = mongoose; */
+
+export const connect = async () => {
+
+    if (mongoConnection.isConnected) {
+        console.log('Ya estabamos conectados');
+        return;
+    }
+
+    if (mongoose.connections.length > 0) {
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
+
+        if (mongoConnection.isConnected === 1) {
+            console.log('Usando conexión anterior');
+            return;
+        }
+
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect(process.env.MONGO_URL || '');
+    mongoConnection.isConnected = 1;
+    console.log('Conectado a MongoDB:', process.env.MONGO_URL);
+}
+
+export const disconnect = async () => {
+
+    if (process.env.NODE_ENV === 'development') return;
+
+    if (mongoConnection.isConnected === 0) return;
+
+    await mongoose.disconnect();
+    mongoConnection.isConnected = 0;
+
+    console.log('Desconectado de MongoDB');
+}
