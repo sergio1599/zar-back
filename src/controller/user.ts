ae3c7b48247d21473768385c6f84a55f4dde041a
Sergio Quintana <sergioq15@hotmail.com>
2023-02-08 10:47:08 -0500

    feat: add methods for user in the controller

---
+++ src/controller/user.ts
@@ -0,0 +1,134 @@
+import { Request, Response } from 'express';
+/* import { generate } from 'generate-password-ts'; */
+/* import { genSaltSync, hashSync } from 'bcrypt'; */
+import { database } from '../database';
+import User from '../models/User';
+
+
+export const getUser = async (req: Request, res: Response) => {
+    await database.connect();
+    const { email } = req.params;
+    try {
+        const user = User.findOne({ email });
+        if (!user) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+        return res.status(200).json({
+            user
+        });
+    } catch (error) {
+        await database.disconnect();
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+
+export const getUsers = async (_req: Request, res: Response) => {
+    try {
+        await database.connect();
+        const users = await User.find({});
+        if (!users) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'Users not found'
+            });
+        }
+        return res.status(200).json({
+            users
+        });
+    } catch (error) {
+        await database.disconnect();
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    await database.connect();
+    const { name, email, password, role } = req.body;
+    try {
+        const user = new User({
+            name,
+            email,
+            password,
+            role
+        });
+        await user.save();
+        await database.disconnect();
+        return res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            user
+        });
+
+    } catch (error) {
+        await database.disconnect();
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        await database.connect();
+        const { email } = req.params;
+        const user = await User.findByIdAndUpdate({ email }, req.body, { new: true });
+        if (!user) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+        await database.disconnect();
+        return res.status(200).json({
+            success: true,
+            message: 'User updated successfully',
+            user
+        });
+    } catch (error) {
+        await database.disconnect();
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        await database.connect();
+        const { email } = req.params;
+        const user = await User.findOneAndDelete({ email });
+        if (!user) {
+            await database.disconnect();
+            return res.status(400).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+        await database.disconnect();
+        return res.status(200).json({
+            success: true,
+            message: 'User deleted successfully',
+        });
+    } catch (error) {
+        await database.disconnect();
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
